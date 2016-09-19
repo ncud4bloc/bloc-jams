6c52a81ca80b6d30508ac4bbdb9e0d2da7eba08c
Neil Cudden <ncudden@sbcglobal.net>
2016-09-19 16:18:24 -0700

    Assignment-8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,7 @@ var animatePoints = function() {
  
     var points = document.getElementsByClassName('point');
  
-    var revealFirstPoint = function() {
+/*    var revealFirstPoint = function() {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
         points[0].style.msTransform = "scaleX(1) translateY(0)";
@@ -26,5 +26,23 @@ var animatePoints = function() {
     revealFirstPoint();
     revealSecondPoint();
     revealThirdPoint();
- 
+    */
+    
+/* Neil Cudden Stuff 
+Added a 45 degree rotation 
+Changed the transition behaviour in landing.css */
+    
+    var revealPoint = function(){
+       for(var i=0;i<3;i++){
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0) rotate(45deg)";
+        points[i].style.msTransform = "scaleX(1) translateY(0) rotate(45deg)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0) rotate(45deg)";
+        
+           
+       } 
     };
+
+    
+    revealPoint(); 
+};
