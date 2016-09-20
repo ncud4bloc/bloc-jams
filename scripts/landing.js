d53022609c5f27976d443f8b9d2cab996eee05d3
Neil Cudden <ncudden@sbcglobal.net>
2016-09-19 17:02:53 -0700

    Assignment-8 with js script corrections

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,48 +1,20 @@
 var animatePoints = function() {
  
     var points = document.getElementsByClassName('point');
- 
-/*    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
- 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
- 
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
- 
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-    */
     
 /* Neil Cudden Stuff 
 Added a 45 degree rotation 
 Changed the transition behaviour in landing.css */
     
-    var revealPoint = function(){
-       for(var i=0;i<3;i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0) rotate(45deg)";
-        points[i].style.msTransform = "scaleX(1) translateY(0) rotate(45deg)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0) rotate(45deg)";
+    var revealPoint = function(index){
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0) rotate(45deg)";
+        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(45deg)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0) rotate(45deg)";
         
-           
-       } 
     };
 
-    
-    revealPoint(); 
+    for(var i=0;i<points.length;i++){
+        revealPoint(i); 
+    }
 };
