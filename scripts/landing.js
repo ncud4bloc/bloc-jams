f14eee2d8cb64c7c7d085715386f336c7bc12b9c
Neil Cudden <ncudden@sbcglobal.net>
2016-09-22 09:44:13 -0700

    Checkpoint-9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,16 @@
-var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-    var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
     
 /* Neil Cudden Stuff 
-Added a 45 degree rotation 
-Changed the transition behaviour in landing.css */
+My 45 degree rotation now commented out 
+*/
     
     var revealPoint = function(index){
         points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0) rotate(45deg)";
-        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(45deg)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0) rotate(45deg)";
+        points[index].style.transform = "scaleX(1) translateY(0)"; // rotate(45deg)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)"; //  rotate(45deg)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; //  rotate(45deg)";
         
     };
 
@@ -18,3 +18,19 @@ Changed the transition behaviour in landing.css */
         revealPoint(i); 
     }
 };
+
+ window.onload = function() {
+        // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
