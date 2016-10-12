b047bb43d0015a214389e44edf794524e0b0ec74
Neil Cudden <ncudden@sbcglobal.net>
2016-10-12 11:02:01 -0700

    Assignment-19 Redo completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,11 @@
+var setSong = function (songNumber) {
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+};
+
+var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+};
 
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -16,21 +24,19 @@
 
 	        if (currentlyPlayingSongNumber !== null) {
 		          // Revert to song number for currently playing song because user started playing new song.
-		          var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		          var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);   //Ncc Assignment-19 update
                   currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	        }
             if (currentlyPlayingSongNumber !== songNumber) {
 		          // Switch from Play -> Pause button to indicate new song is playing.
 		          $(this).html(pauseButtonTemplate);
-                  currentlyPlayingSongNumber = songNumber;
-                  currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+                  setSong(songNumber);                                      //Ncc Assignment-19 update
                   updatePlayerBarSong();
             } else if (currentlyPlayingSongNumber === songNumber) {
 		          // Switch from Pause -> Play button to pause currently playing song.
 		          $(this).html(playButtonTemplate);
                   $('.main-controls .play-pause').html(playerBarPlayButton);
-		          currentlyPlayingSongNumber = null;
-                  currentSongFromAlbum = null;
+		          setSong(songNumber);                                      //Ncc Assignment-19 update
 	        }
      };
  
